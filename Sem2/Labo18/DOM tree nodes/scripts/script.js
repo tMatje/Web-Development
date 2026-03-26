7c41d62bd486834cb5f78473b87d4fb12f1d2366
tMatje <matthias.bonte2@student.vives.be>
2026-03-26 12:56:39 +0100

    game

---
+++ Sem2/Labo18/DOM tree nodes/scripts/script.js
@@ -0,0 +1,4 @@
+const setup = () => {
+    let txt = document.getElementById('abc')
+}
+window.addEventListener("load", setup);
---
+++ script.js
@@ -0,0 +1,5 @@
+function toggleGame() {
+    document.getElementById("game-container").classList.toggle("expanded");
+}
+
+window.addEventListener("load", setup);
